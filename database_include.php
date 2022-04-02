6479222460d2865a6356e760d0178fdc98695dea
nicolekarlin <101667351+nicolekarlin@users.noreply.github.com>
2022-04-02 21:36:54 +0200

    Erstellen der database_include.php und Verbindung zur Datenbank auf index.php

---
+++ database_include.php
@@ -0,0 +1,3 @@
+<?php
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ap121', 'ap121', 'evi7ohS5ea', array('charset'=>'utf8'));
+?>
--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
+require("./database_include.php");
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Titel</title>
+</head>
+<body>
+
+
 "Hello
 Blablalbla"
- ?>
+
+</body>
+
+</html>
+
