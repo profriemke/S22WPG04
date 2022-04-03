db9cbb71e4f8d380f9cc792360ea66da91810c32
nicolekarlin <101667351+nicolekarlin@users.noreply.github.com>
2022-04-03 19:04:14 +0200

    Erstellen einer API die die Rezepte in JSON ausgibt

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 require("./database_include.php");
 ?>
 <!DOCTYPE html>
-<html lang="de">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Titel</title>
+    <title>Webprojekt</title>
 </head>
+
 <body>
 
-
-"Hello
-Blablalbla"
+    <?php
+    $statement = $pdo->prepare('SELECT * FROM Rezepte');
+    if($statement->execute()) {
+        header("Content-Type: application/json");
+        $data=$statement->fetchAll(PDO::FETCH_ASSOC);
+        echo json_encode($data, JSON_UNESCAPED_UNICODE);
+    } else {
+        echo 'Datenbank-Fehler:';
+        echo $statement->errorInfo()[2];
+        echo $statement->queryString;
+        die();
+    }
+    ?>
 
 </body>
 
