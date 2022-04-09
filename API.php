8cee9e754f35aa35e000207ad7a5627b5fb88e1e
nicolekarlin <101667351+nicolekarlin@users.noreply.github.com>
2022-04-09 12:58:30 +0200

    Abänderung Code bei "else"

--- API.php
+++ API.php
@@ -9,9 +9,7 @@ if($statement->execute()) {
     $data=$statement->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($data, JSON_UNESCAPED_UNICODE);
 } else {
-    echo 'Datenbank-Fehler:';
-    echo $statement->errorInfo()[2];
-    echo $statement->queryString;
+    echo 'Datenbank Fehler, bitte erneut versuchen';
     die();
 }
 ?>
