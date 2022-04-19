4313c9cf0daf4ccff675a2536d6846b087f7248d
julianu12 <ju009@hdm-stuttgart.de>
2022-04-19 18:24:46 +0200

    Basis für Kategorien auf Startseite hinzugefügt

--- php/public_sites/index.php
+++ php/public_sites/index.php
@@ -10,6 +10,8 @@ include("../../database_include.php");
 </head>
 <body>
 <?php
+//Alle Rezepte
+echo " <h1> Entdecken </h1>";
 $statement = $pdo->prepare('SELECT * FROM Rezepte');
 if ($statement->execute()) {
     while ($row = $statement->fetch()) {
@@ -38,12 +40,69 @@ if ($statement->execute()) {
 else {
     die("Dieses Rezept ist aktuell leider nicht verfügbar.");
 }
+/*
+//Neue Rezepte
+echo " <h1> Neue Rezepte </h1>";
+$statement = $pdo->prepare('SELECT * FROM Rezepte ORDER BY TIMESTAMP DESC');
+if ($statement->execute()) {
+    while ($row = $statement->fetch()) {
+        //$datei="../../files".$row["datei"];
+        echo "<div class='titel'>";
+        echo "<h3>";
+        echo htmlspecialchars($row['Titel']);
+        echo "</h3>";
+        echo "<br>";
+        echo "<p class='Inhalt'>";
+        echo htmlspecialchars($row['Inhalt']);
+        echo "</p>";
+        echo "<br>";
+        # echo "<div class='postpicture'>";
+        #if (!empty($row["datei"])){
+        #   echo "<img src='../../".$row["datei"]. "'>";
+        # }
+
+        #echo "</div>";
+        #echo "<br>";
+        echo "<a href='rezept_edit.php?id=".$row["ID"]." ' class='button'> Rezept bearbeiten </a>";
+        #echo "<a href='../post/delete_do.php?id=".$row["id"]."' class='button'> Post löschen </a>";
+        #echo "</div>";
+    }
+}
+else {
+    die("Dieses Rezept ist aktuell leider nicht verfügbar.");
+}
+// Schnelle Rezepte
+echo " <h1> Schnelle Rezepte </h1>";
+$statement = $pdo->prepare('SELECT * FROM Rezepte');
+if ($statement->execute()) {
+    while ($row = $statement->fetch()) {
+        //$datei="../../files".$row["datei"];
+        echo "<div class='titel'>";
+        echo "<h3>";
+        echo htmlspecialchars($row['Titel']);
+        echo "</h3>";
+        echo "<br>";
+        echo "<p class='Inhalt'>";
+        echo htmlspecialchars($row['Inhalt']);
+        echo "</p>";
+        echo "<br>";
+        # echo "<div class='postpicture'>";
+        #if (!empty($row["datei"])){
+        #   echo "<img src='../../".$row["datei"]. "'>";
+        # }
+
+        #echo "</div>";
+        #echo "<br>";
+        echo "<a href='rezept_edit.php?id=".$row["ID"]." ' class='button'> Rezept bearbeiten </a>";
+        #echo "<a href='../post/delete_do.php?id=".$row["id"]."' class='button'> Post löschen </a>";
+        #echo "</div>";
+    }
+}
+else {
+    die("Dieses Rezept ist aktuell leider nicht verfügbar.");
+}
+*/
 ?>
 
-<?php
-
-echo("Neuste Rezepte");
-?>
-?>
 
 </body>
