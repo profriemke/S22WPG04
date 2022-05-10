a5b77eb86002aad10b2d155c58b938a0c00dd687
julianullrch <ju009@hdm-stuttgart.de>
2022-05-10 12:49:23 +0200

    rating formular eingefügt

---
+++ php/rating.php
@@ -0,0 +1,34 @@
+<?php
+include("./includes/database_include.php");
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Bewertung</title>
+</head>
+
+<body>
+
+    <form action="rating_do.php" method="post" >
+        <input type="radio" name="rating" value=5 id="rating-5">
+        <label for="rating-5"></label>
+        <input type="radio" name="rating" id="rating-4">
+        <label for="rating-4"></label>
+        <input type="radio" name="rating" id="rating-3">
+        <label for="rating-3"></label>
+        <input type="radio" name="rating" id="rating-2">
+        <label for="rating-2"></label>
+        <input type="radio" name="rating" id="rating-1">
+        <label for="rating-1"></label>
+
+        <label for="comment"> Schreibe einen Kommentar </label>
+        <textarea cols="20" name="comment="25" placeholder="Schreibe einen Kommentar" id="comment"></textarea>
+
+        <button type="submit">Bewertung abgeben</button>
+
+    </form>
+</body>
+</html>
