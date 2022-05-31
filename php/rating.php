b6cbb5636d08fed2769ef2fff46fef45dc652268
julianullrch <ju009@hdm-stuttgart.de>
2022-05-31 12:48:51 +0200

    rating update

--- php/rating.php
+++ php/rating.php
@@ -1,5 +1,6 @@
 <?php
-include("./includes/database_include.php");
+#include("./includes/database_include.php");
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ap121', 'ap121', 'evi7ohS5ea', array('charset'=>'utf8'));
 session_start();
 ?>
 
@@ -11,23 +12,34 @@ session_start();
 </head>
 
 <body>
+<?php
+    $nutzer_id=$_SESSION['id'];
+    $recipe_id=$_POST['recipe_id'];
+?>
 
     <form action="rating_do.php" method="post" >
-        <input type="radio" name="rating" value=5 id="rating-5">
-        <label for="rating-5"></label>
-        <input type="radio" name="rating" id="rating-4">
-        <label for="rating-4"></label>
-        <input type="radio" name="rating" id="rating-3">
-        <label for="rating-3"></label>
-        <input type="radio" name="rating" id="rating-2">
-        <label for="rating-2"></label>
-        <input type="radio" name="rating" id="rating-1">
-        <label for="rating-1"></label>
+        <input type="hidden" name = "user_id" value="<?php echo($nutzer_id);?>">
 
-        <label for="comment"> Schreibe einen Kommentar </label>
-        <textarea cols="20" name="comment="25" placeholder="Schreibe einen Kommentar" id="comment"></textarea>
+        <input type="hidden" name="recipe_id" value="<?php echo($recipe_id);?>">
+        <input type="hidden" name="rezept_id" value=1>
 
-        <button type="submit">Bewertung abgeben</button>
+
+        <input type="radio" name="rating" value="5" id="rating-5">
+        <label for="rating-5">5</label>
+        <input type="radio" name="rating" value="4" id="rating-4">
+        <label for="rating-4">4</label>
+        <input type="radio" name="rating" value="3" id="rating-3">
+        <label for="rating-3">3</label>
+        <input type="radio" name="rating" value="2" id="rating-2">
+        <label for="rating-2">2</label>
+        <input type="radio" name="rating" value="1" id="rating-1">
+        <label for="rating-1">1</label>
+
+        <textarea cols="20" name="comment" placeholder="Schreibe einen Kommentar" id="comment"></textarea>
+
+        <input type ="hidden" name="timestamp" value="">
+
+        <input type="submit" value = "Bewertung abgeben" >
 
     </form>
 </body>
---
+++ php/rating_do.php
@@ -0,0 +1,47 @@
+<?php
+#include("./includes/database_include.php");
+$pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de; dbname=u-ap121', 'ap121', 'evi7ohS5ea', array('charset'=>'utf8'));
+session_start();
+
+//if (!isset($_SESSION["id"])) {
+//    echo "<a href='../public_sites/index.php'> Zurück zur Startseite <br></a>";
+//    die("Um diese Aktion ausführen zu können musst du angemeldet sein!");
+
+//}
+$sterne = $_POST["rating"];
+$nutzer_id=$_SESSION['id'];
+if (isset($_POST["comment"]) && isset($_POST["rating"])) {
+    $statement = $pdo->prepare("INSERT INTO Bewertungen (rezept_id, nutzer_id, rating, kommentar, timestamp) VALUES  (?, ?, ?, ?, NOW())"); #hier sterne zu rating angleichen, vorher datenbank überprüfen
+    if ($statement->execute(array( htmlspecialchars($_POST["rezept_id"]), htmlspecialchars($_POST["id"]), htmlspecialchars($_POST["rating"]), htmlspecialchars($_POST["kommentar"]))))
+        echo("Bewertung eingefügt");
+
+    else{
+        echo $statement->errorInfo()[2];
+        echo $statement->queryString;
+        die("Fehler beim Einfügen");
+    }
+        }
+/*
+ if(isset($_POST["rating"])&& isset($_POST["comment"])){
+    $rating = $_POST["rating"];
+    $comment = $_POST["comment"];
+    #$id=$_SESSION['id'];
+
+    $statement = $pdo->prepare("INSERT INTO Bewertungen (sterne, kommentar, nutzer_id) VALUES (:rating, :comment)");
+
+    $statement->bindParam(':rating', $rating);
+    $statement->bindParam(':comment', $_comment);
+
+
+    if($statement->execute() ){
+        echo "Upload erfolgreich";
+    }
+    else {
+        echo $statement->errorInfo()[2];
+        echo $statement->queryString;
+        echo "</div>";
+        die();
+    }
+    }
+*/
+
