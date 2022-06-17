7e854125d16c7c624735ee6ba50f154cd4c684ed
nicolekarlin <101667351+nicolekarlin@users.noreply.github.com>
2022-06-17 15:23:17 +0200

    Anpassungen CSS

--- php/nutzer_management/login.php
+++ php/nutzer_management/login.php
@@ -15,17 +15,17 @@ session_start();
 require("../includes/navbar_include.php");
 ?>
 <body>
-<h2> Bei eat.pray.eat anmelden </h2>
-<div>
-<form action="login_do.php" method="post">
 
-    <input type="text" name="username" id="username" placeholder="Username"><br>
-    <input type="password" name="passwort" id="passwort" placeholder="Passwort"> <br>
+<div class="content post">
+    <h2> Bei eat.pray.eat anmelden </h2>
+<form class="signup-wrapper" action="login_do.php" method="post">
 
-    <button type="submit" id="absenden">anmelden</button>
+    <input type="text" name="username" id="username" minlength="4" required placeholder="Username"><br>
+    <input type="password" name="passwort" id="passwort"  minlength="4" required placeholder="Passwort"> <br>
+
+    <button class="action-button" type="submit" id="absenden">anmelden</button>
 </form>
-</div>
-<div>
+
 <p> Noch kein Konto? Jetzt <a href="registrieren.php">registrieren</a>! </p>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
