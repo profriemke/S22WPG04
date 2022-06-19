016e479c00789b61b44920fdffe24e548852bc7e
julianullrch <ju009@hdm-stuttgart.de>
2022-06-19 17:05:08 +0200

    Bugfixes an Karussell und Navibar

--- php/oeffentliche_seiten/index.php
+++ php/oeffentliche_seiten/index.php
@@ -167,7 +167,7 @@ else {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 </body>
-
+<!---->
 <?php
 include("./../includes/footer_include.php");
 
