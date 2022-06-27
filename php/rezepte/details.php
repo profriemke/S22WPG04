21d5b960db52c8100eb0159f6af7006cd30be20a
julianullrch <ju009@hdm-stuttgart.de>
2022-06-27 21:26:46 +0200

    Struktur für Kommentare eingefügt

--- php/rezepte/details.php
+++ php/rezepte/details.php
@@ -6,6 +6,7 @@
     <title>Rezept</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/rating.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" >
 
 
@@ -75,7 +76,7 @@ if($statement->execute()) {
             if($statement->execute()) {
                 while ($row = $statement->fetch()) {
                     /*echo "<div style='border-width: 5px; border-color: dimgrey'>";*/
-
+                    echo('<div class="kommentar">');
                         echo "Bewertung:";
                         echo htmlspecialchars($row['rating']);
                         echo "<br>";
@@ -84,7 +85,7 @@ if($statement->execute()) {
                         echo htmlspecialchars($row['kommentar']);
                         echo "</p>";
                         echo "<br>";
-
+                    echo('</div>');
                    /* echo "</div>";*/
                 }
 
